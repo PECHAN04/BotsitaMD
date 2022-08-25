70f51b09951e3d596ab20ec815d9ebd4fa403119
PECHAN04 <103233109+PECHAN04@users.noreply.github.com>
2022-08-24 21:47:13 -0500

    Update info-cuentas-oficiales.js

--- plugins/info-cuentas-oficiales.js
+++ plugins/info-cuentas-oficiales.js
@@ -4,17 +4,7 @@ let str = `
 💙 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝘼𝙎 𝘾𝙐𝙀𝙉𝙏𝘼𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎
 💜 𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙏𝙊 𝙏𝙃𝙀 𝙊𝙁𝙁𝙄𝘾𝙄𝘼𝙇 𝘼𝘾𝘾𝙊𝙐𝙉𝙏𝙎
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ *GITHUB*
-*${md}*
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ *INSTAGRAM - ASISTENCIA*
-*${ig}*
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ *YOUTUBE*
-*${yt}*
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-*Si tienen dudas, sugerencias, o preguntas solo escriban por Instagram.*\n
-*If you have doubts, suggestions or questions just write on Instagram.*
+✅ *zzz*
 `.trim()
   
 conn.sendHydrated(m.chat, str, wm, media, 'https://github.com/GataNina-Li/GataBot-MD', '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿', null, null, [
